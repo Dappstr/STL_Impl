5dcf369526bafd058e6097768d14fc5eb6d20786
Lane Beals <lanebeals02@gmail.com>
2023-05-20 22:59:44 -0700

    Started string header file

--- include/string.h
+++ include/string.h
@@ -1,4 +1,16 @@
-// TODO:
-// Implement core string functionality (constructors, destructors)
-// Implement operators
-// Implement helper functions
+#pragma once
+
+#include <iostream>
+
+class String
+{
+    private:
+        int m_size = 0;
+        char* buffer = nullptr;
+    
+    public:
+        //TODO
+        // Implement core string functionality (constructors, destructors)
+        // Implement operators
+        // Implement helper functions
+};
