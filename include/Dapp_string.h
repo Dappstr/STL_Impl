70c5063a2feb39b3f93a1359d71d158ebac6ad23
Lane Beals <lanebeals02@gmail.com>
2023-05-21 09:30:45 -0700

    Added constructors

--- include/Dapp_string.h
+++ include/Dapp_string.h
@@ -1,6 +1,7 @@
 #pragma once 
 #include <cstring> // This imports strlen and strcpy functions
 #include <iostream>
+#include <utility> // std::move
 
 class String
 {
@@ -11,15 +12,35 @@ class String
     public:
         //TODO
         // Implement core string functionality (constructors, destructors)
-       String() {
+       explicit String() {
            m_buffer = new char[1];
            m_buffer[0] = '\0';
        }
-
+        
+       //Explicit constructor to only allow either empty construction, or construction of strings
+       //This disallows users from passing something like an integer or double as a constructor parameter
        explicit String(const char* str)
-       :m_size(strlen(str)) { 
+           :m_size(strlen(str)) { 
            m_buffer = new char[m_size + 1];
            strcpy(m_buffer, str);
+       }
+
+       //Copy constructor
+       String(const String& src)
+           :m_size(src.m_size) {
+           strcpy(m_buffer, src.m_buffer);
+       }
+
+       //Move constructor
+       String(String&& src)
+           :m_size(std::move(src.m_size)) {
+           m_buffer = std::move(src.m_buffer); 
+           
+            //We set the source string to have no value.
+            //This could differ from the STL implementation of string where after moving, the object is in a valid but undefined state, however,
+            //I feel as though we should be able to define the state clearly, which is that it is empty
+           src.m_buffer = nullptr;
+           src.m_size = 0;
        }
 
        ~String()
