dd64fb5e0365927fbb4c0738f0ca00526d120667
Dappster <87837549+Dappstr@users.noreply.github.com>
2023-05-22 15:23:05 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include "../include/Dapp_string.hpp"
 #include "../include/Dapp_for_each.hpp"
+#include "../include/Dapp_vector.hpp"
 
 int main(int argc, char* argv[])
 {
+    
+    Vector<int, 5> vec = {1, 2, 3, 4, 5};
+    vec.pop();
+    const int lval = 6;
+    vec.append(lval);
 
-    String str = "Hello";
-    for_each(str.begin(), str.end(), [](char c) {std::cout << c << ' ';});
+    std::cout << vec;
     return 0;
 }
