3f0b5effba51fb23cb0bc01cba3e70f71ae7ccb4
Dappster <87837549+Dappstr@users.noreply.github.com>
2023-05-22 15:22:48 -0700

    Update Dapp_vector.hpp

--- include/Dapp_vector.hpp
+++ include/Dapp_vector.hpp
@@ -1,6 +1,8 @@
 #include <cassert>
+#include <cstdlib>
 #include <iostream>
 #include <memory>
+#include <stdlib.h>
 
 template <typename T, size_t N = 0>
 class Vector
@@ -14,19 +16,31 @@ class Vector
 
         // Constructors
         Vector() {
-            m_buffer = new T[sizeof(T)];
+            m_buffer = new T[N];
             m_size = N;
+            m_cap = N;
         }
 
-        Vector (size_t n){ m_buffer = new T[n]; }
-
+        Vector (size_t n) {
+            m_buffer = new T[n];
+            m_size = n;
+            m_cap = n;
+        }
+        
+        Vector(std::initializer_list<T> lst) {
+            m_size = lst.size();
+            m_cap = lst.size();
+            m_buffer = new T[m_cap];
+            std::copy(lst.begin(), lst.end(), this->begin());
+        }
+         
         // Utility functions
         const size_t size() const { return m_size; } // Will return current size
         const size_t capacity() const {return m_cap; } // Will return maximum current capacity
         const bool empty() { return m_size > 0; }
 
         auto* begin() const noexcept(noexcept(this->m_size > 0)) {
-            assert(std::addressof(m_buffer != NULL));
+            assert(m_buffer != NULL);
             return &m_buffer[0];
         }
         
@@ -34,7 +48,36 @@ class Vector
             assert(m_size >= 0);
             return &m_buffer[m_size-1];
         }
-        
+       
+        void append(const T& val) {
+            if (m_size == m_cap) {
+                T new_buff = new T[m_size + 1];
+                memcpy(new_buff, m_buffer, m_size);
+                new_buff[m_size+1] = val;
+                delete[] m_buffer;
+                ++m_size;
+                m_buffer = new_buff;
+            }
+            else {
+                m_buffer[m_size-1] = val;
+            }
+        }
+
+
+        auto pop() {
+            assert(m_size > 0 && "Out of index error!\n");
+            T popped = m_buffer[m_size-1];            
+            --m_size;
+            
+            if(m_size > 0) {
+                T* new_buffer = new T[m_size];
+                memcpy(new_buffer, m_buffer, m_size * sizeof(T));
+                delete[] m_buffer;
+                m_buffer = new_buffer;
+            }
+            return popped;
+        }
+
         // Operators
         friend std::ostream& operator << (std::ostream& out, Vector<T, N>& vec ) {
             for(int i = 0; i < vec.m_size; ++i) {
