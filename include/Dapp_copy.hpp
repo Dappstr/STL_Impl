6500b1c5938d1a9dcd00e2a4b2a282fd2092abcb
Dappster <87837549+Dappstr@users.noreply.github.com>
2023-05-27 13:45:30 -0700

    Create Dapp_copy.hpp

---
+++ include/Dapp_copy.hpp
@@ -0,0 +1,25 @@
+#include <concepts>
+#include <type_traits>
+
+#if __cplusplus == 202002L
+
+template <std::input_or_output_iterator InputIt, std::output_iterator<typename std::iterator_traits<InputIt>::value_type> OutputIt>
+requires std::copyable<typename std::iterator_traits<InputIt>::value_type>
+OutputIt my_copy(InputIt first, InputIt last, OutputIt d_first) {
+    while (first != last) {
+        *d_first++ = *first++;
+    }
+    return d_first;
+}
+
+#elif __cplusplus <= 201703L
+
+template <typename InputIt, typename OutputIt>
+std::enable_if_t<std::is_copy_assignable<typename std::iterator_traits<InputIt>::value_type>::value, OutputIt>
+my_copy(InputIt first, InputIt last, OutputIt d_first) {
+    while (first != last) {
+        *d_first++ = *first++;
+    }
+    return d_first;
+}
+#endif
