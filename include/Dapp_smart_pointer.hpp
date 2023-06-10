e17d0510af311bf2557f9f317a705a28d8da7c44
Dappster <87837549+Dappstr@users.noreply.github.com>
2023-06-09 23:34:31 -0700

    Create Dapp_smart_pointer.hpp

---
+++ include/Dapp_smart_pointer.hpp
@@ -0,0 +1,42 @@
+//WIP
+
+template <typename T>
+class Smart_ptr
+{
+    private:
+        T* m_ptr = nullptr;
+
+    public:
+        Smart_ptr(T* ptr = nullptr) : m_ptr(ptr) {}
+
+        //deleting copy constructor and copy assignment operator to replicate unique_ptr
+        Smart_ptr(const Smart_ptr<T>&) = delete;
+        Smart_ptr& operator=(const Smart_ptr<T>&) = delete;
+
+        Smart_ptr(Smart_ptr<T>&& dst) {
+            if(this != dst) {
+                delete m_ptr;
+                m_ptr = dst.m_ptr;
+                dst.m_ptr = nullptr;
+            }
+            return *this;
+        }
+
+        Smart_ptr& operator=(Smart_ptr<T>&& dst) {
+            if(this != dst) {
+                delete m_ptr;
+                m_ptr = dst.m_ptr;
+                dst.m_ptr = nullptr;
+            }
+            return *this;
+        }
+
+        T& get_value() { return *m_ptr; }
+        T& operator*() const { return *m_ptr; }
+
+        T* get() { return m_ptr; }
+        T* operator->() const { return m_ptr; }
+
+
+        ~Smart_ptr() { delete m_ptr; }
+};
