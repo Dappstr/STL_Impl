e8fbd4731c92bf6bc9f66e931115a5acede15586
Lane Beals <lanebeals02@gmail.com>
2023-11-16 13:43:40 -0800

    Oops

--- include/Dapp_replace_if.h
+++ include/Dapp_replace_if.h
@@ -2,7 +2,7 @@
 
 namespace dapp {
     template <typename Iter, typename Pred, typename T>
-    void my_replace_if(Iter start, Iter end, Pred pred, T new_value) {
+    void replace_if(Iter start, Iter end, Pred pred, T new_value) {
         for(; start != end; ++start) {
             if(pred(*start)) {
                 *start = new_value;
