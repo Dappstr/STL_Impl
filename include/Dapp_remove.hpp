05a85764cf2a14b35d18814a6550af80fd1eadc0
Lane Beals <lanebeals02@gmail.com>
2023-11-16 15:10:07 -0800

    Oops, forgot header guard and namespace

--- include/Dapp_remove.hpp
+++ include/Dapp_remove.hpp
@@ -1,17 +1,20 @@
-template <typename Iter, typename T>
-void remove(Iter start, Iter end, T value) {
-    while(start != end) {
-        if(*start == value) {
-            decltype(auto) next = start;
-            while(next + 1 != end) {
-                *next = *(next + 1);
-                ++next;
+#pragma once
+
+namespace dapp {
+    emplate<typename Iter, typename T>
+    void remove(Iter start, Iter end, T value) {
+        while (start != end) {
+            if (*start == value) {
+                decltype(auto) next = start;
+                while (next + 1 != end) {
+                    *next = *(next + 1);
+                    ++next;
+                }
+                *next = 0;
+                --end;
+            } else {
+                ++start;
             }
-            *next = 0;
-            --end;
-        }
-        else {
-            ++start;
         }
     }
 }
--- include/Dapp_remove_if.hpp
+++ include/Dapp_remove_if.hpp
@@ -1,17 +1,20 @@
-template <typename Iter, typename Pred>
-void remove_if(Iter start, Iter end, Pred pred) {
-    while(start != end) {
-        if(pred(*start)) {
-            decltype(auto) next = start;
-            while(next + 1 != end) {
-                *next = *(next + 1);
-                ++next;
+#pragma once
+
+namespace dapp {
+    template<typename Iter, typename Pred>
+    void remove_if(Iter start, Iter end, Pred pred) {
+        while (start != end) {
+            if (pred(*start)) {
+                decltype(auto) next = start;
+                while (next + 1 != end) {
+                    *next = *(next + 1);
+                    ++next;
+                }
+                *next = 0;
+                --end;
+            } else {
+                ++start;
             }
-            *next = 0;
-            --end;
-        }
-        else {
-            ++start;
         }
     }
 }
