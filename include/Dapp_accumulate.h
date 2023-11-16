eb14cbcc2165ae5369f346fc5846e82eaec1f2f8
Lane Beals <lanebeals02@gmail.com>
2023-11-15 21:52:13 -0800

    Changed SFINAE to be in template parameter args

--- include/Dapp_accumulate.h
+++ include/Dapp_accumulate.h
@@ -1,11 +1,11 @@
 #include <type_traits>
 #include <iterator>
 
-template <typename Iter, typename T>
+template <typename Iter, typename T,
+        typename = std::enable_if_t<
+                        std::is_arithmetic<T>::value &&
+                        std::is_arithmetic<typename std::iterator_traits<Iter>::value_type>::value, T>>
 auto accumulate(Iter begin, Iter end, T start)
-    -> std::enable_if_t<
-        std::is_arithmetic<T>::value &&
-        std::is_arithmetic<typename std::iterator_traits<Iter>::value_type>::value, T>
 {
     T sum = start;
     for (; begin != end; ++begin) { // Corrected loop condition
